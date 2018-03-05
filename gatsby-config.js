27968cb254cdc1f1adc332c5ca998c3dafb66491
Anuj Malla <anujmalla@gmail.com>
2018-03-04 21:47:37 -0800

    Google Analtyics

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,5 +23,15 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: process.env.TRACKING_ID,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+      },
+    },
   ],
 };
